b76edb4192018f3b4da87a3c3d8c3ed0cf85a63b
Cameron Beanland <cameronbeanland@hotmail.com>
2025-02-28 19:52:50 -0330

    Update CityRepo.java
    Added an ungodly amount of imports to each of the following, and added a custom query to help define relationships between them (interactions with each other)

--- server/src/main/java/com/example/demo/repository/CityRepo.java
+++ server/src/main/java/com/example/demo/repository/CityRepo.java
@@ -1,4 +1,12 @@
 package com.example.demo.repository;
 
-public class CityRepo {
+import com.example.demo.model.Airport;
+import com.example.demo.model.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CityRepo extends JpaRepository<City, Long> {
+    // custom query to obtain all airports in a specific city
+    List<Airport> findAirportsByCityId(Long cityId);
 }
