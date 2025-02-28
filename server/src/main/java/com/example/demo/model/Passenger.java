1e1aa0fa9b1cab027add8368071da7224d69e9b5
Cameron Beanland <cameronbeanland@hotmail.com>
2025-02-28 17:25:44 -0330

    Update Passenger.java
    Added lombok.Data and NoArgsConstructor to auto-generate accessors/mutators, and anything else I didn't want to code LOL

--- server/src/main/java/com/example/demo/model/Passenger.java
+++ server/src/main/java/com/example/demo/model/Passenger.java
@@ -5,11 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Passenger class for helping people book flights, keeps track of their name and phone number
  */
 @Entity  // mark as JPA entity
+@Data // generate the mutator/accessors, toString, any hashCode and equals
+@NoArgsConstructor // generates a default constructor, to reduce code size
 public class Passenger {
     @Id  // pk
     @GeneratedValue(strategy = GenerationType.IDENTITY)  // auto-gen ID
