a3ec9f6409155b22ac91dfa21dbdb33e44c45d38
Cameron Beanland <cameronbeanland@hotmail.com>
2025-02-28 17:03:18 -0330

    Update Aircraft.java
    Imported Jakarta for use of Entity, Id, GeneratedValue and GenerationType annotations. Overall purpose is to:
    
    - @entity indicates that the given class is an entity to be used in a database
    @id marks the Primary Key (PK) in said entity class
    @GeneratedValue will specify how the PK is generated, i.e generated automatically
    @manytoone, OneToMany defines relationships between the entities
    
    Besides that, I added the private objects and haven't touched the mutators/accessors, which are next. (this commit desc. will be C&P'd for the other classes so you can safely ignore the other commits)

--- server/src/main/java/com/example/demo/model/Aircraft.java
+++ server/src/main/java/com/example/demo/model/Aircraft.java
@@ -1,4 +1,21 @@
 package com.example.demo.model;
 
+// imported jakarta instead of javax due to syntax errors on my end
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+/**
+ * Aircraft that allows Passengers to travel
+ */
+@Entity  // Defines this class as a database entity
 public class Aircraft {
+    @Id  // pk
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // auto-gen ID
+    private Long id;
+    private String type;  // type of aircraft
+    private String airlineName;  // company that owns airline
+    private int numberOfPassengers;
 }
+
