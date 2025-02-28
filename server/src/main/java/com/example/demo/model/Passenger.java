1445f774e4a5d09472729fe244e9d04bc0b34e54
Cameron Beanland <cameronbeanland@hotmail.com>
2025-02-28 17:04:33 -0330

    Update Passenger.java
    Imported Jakarta for use of Entity, Id, GeneratedValue and GenerationType annotations. Overall purpose is to:
    
    - @Entity indicates that the given class is an entity to be used in a database
    @Id marks the Primary Key (PK) in said entity class
    @GeneratedValue will specify how the PK is generated, i.e generated automatically
    @ManyToOne, OneToMany defines relationships between the entities (just realized I forgot some capital letters on the other commits)
    
    Besides that, I added the private objects and haven't touched the mutators/accessors, which are next. (this commit desc. will be C&P'd for the other classes so you can safely ignore the other commits)

--- server/src/main/java/com/example/demo/model/Passenger.java
+++ server/src/main/java/com/example/demo/model/Passenger.java
@@ -1,4 +1,20 @@
 package com.example.demo.model;
 
+// imported jakarta instead of javax due to syntax errors on my end
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+/**
+ * Passenger class for helping people book flights, keeps track of their name and phone number
+ */
+@Entity  // mark as JPA entity
 public class Passenger {
+    @Id  // pk
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // auto-gen ID
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
 }
