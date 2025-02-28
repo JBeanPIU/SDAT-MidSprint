adbaea53585114c98d514c610e92173968f0cf0e
Cameron Beanland <cameronbeanland@hotmail.com>
2025-02-28 17:02:55 -0330

    Update Airport.java
    Imported Jakarta for use of Entity, Id, GeneratedValue and GenerationType annotations. Overall purpose is to:
    
    - @entity indicates that the given class is an entity to be used in a database
    @id marks the Primary Key (PK) in said entity class
    @GeneratedValue will specify how the PK is generated, i.e generated automatically
    @manytoone, OneToMany defines relationships between the entities
    
    Besides that, I added the private objects and haven't touched the mutators/accessors, which are next. (this commit desc. will be C&P'd for the other classes so you can safely ignore the other commits)

--- server/src/main/java/com/example/demo/model/Airport.java
+++ server/src/main/java/com/example/demo/model/Airport.java
@@ -1,4 +1,19 @@
 package com.example.demo.model;
 
+// imported jakarta instead of javax due to syntax errors on my end
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+/**
+ * An airport class to store info on aircraft and where they travel
+ */
+@Entity  // JPA entity
 public class Airport {
+    @Id  // pk
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // auto-increment ID
+    private Long id;
+    private String name;  // name of the airport
+    private String code;  // signifies a unique airport code (like YYZ for Toronto Pearson for example)
 }
