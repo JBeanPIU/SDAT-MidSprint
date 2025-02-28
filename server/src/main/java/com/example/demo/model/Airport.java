cc5bee9b73c26cf22410bc35f970c4dd42587501
Cameron Beanland <cameronbeanland@hotmail.com>
2025-02-28 17:25:15 -0330

    Update Airport.java
    Added lombok.Data and NoArgsConstructor to auto-generate accessors/mutators, and anything else I didn't want to code LOL

--- server/src/main/java/com/example/demo/model/Airport.java
+++ server/src/main/java/com/example/demo/model/Airport.java
@@ -5,11 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * An airport class to store info on aircraft and where they travel
  */
 @Entity  // JPA entity
+@Data // generate the mutator/accessors, toString, any hashCode and equals
+@NoArgsConstructor // generates a default constructor, to reduce code size
 public class Airport {
     @Id  // pk
     @GeneratedValue(strategy = GenerationType.IDENTITY)  // auto-increment ID
