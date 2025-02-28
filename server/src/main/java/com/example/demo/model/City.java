92c4c358c28be14e9c2fc2cc5183584f0cbbb90c
Cameron Beanland <cameronbeanland@hotmail.com>
2025-02-28 17:00:39 -0330

    Update City.java
    Imported Jakarta for use of Entity, Id, GeneratedValue and GenerationType annotations. Overall purpose is to:
    
    - @Entity indicates that the given class is an entity to be used in a database
    @Id marks the Primary Key (PK) in said entity class
    @GeneratedValue will specify how the PK is generated, i.e generated automatically
    @ManyToOne, OneToMany defines relationships between the entities
    
    Besides that, I added the private objects and haven't touched the mutators/accessors, which are next. (this commit desc. will be C&P'd for the other classes so you can safely ignore the other commits)

--- server/src/main/java/com/example/demo/model/City.java
+++ server/src/main/java/com/example/demo/model/City.java
@@ -1,4 +1,20 @@
 package com.example.demo.model;
 
+// imported jakarta instead of javax due to syntax errors on my end
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+/**
+ * This class is designed to represent cities that hold airports
+ */
+@Entity  // @entity marks class as a JPA entity (which is linked to a database table)
 public class City {
+    @Id  // marking field as primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // auto-gen ID value
+    private Long id;
+    private String name;
+    private String state;
+    private int population;
 }
