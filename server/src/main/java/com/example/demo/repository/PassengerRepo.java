566b8ac959ddd77b574a0416ceaf9639cd816ca6
Cameron Beanland <cameronbeanland@hotmail.com>
2025-02-28 19:53:31 -0330

    Update PassengerRepo.java
    Added an ungodly amount of imports to each of the following, and added a custom query to help define relationships between them (interactions with each other)

--- server/src/main/java/com/example/demo/repository/PassengerRepo.java
+++ server/src/main/java/com/example/demo/repository/PassengerRepo.java
@@ -1,4 +1,20 @@
 package com.example.demo.repository;
 
-public class PassengerRepo {
+import com.example.demo.model.Aircraft;
+import com.example.demo.model.Airport;
+import com.example.demo.model.Passenger;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface PassengerRepo extends JpaRepository<Passenger, Long> {
+    // query to extract all aircraft a passenger has traveled on
+    @Query("SELECT a FROM Aircraft a JOIN a.passengers p WHERE p.id = :passengerId")
+    List<Aircraft> findAircraftsByPassId(@Param("passengerId") Long passengerId);
+
+    // this query is used to get amount of airports a passenger has used
+    @Query("SELECT a FROM Airport a JOIN a.passengers p WHERE p.id = :passengerId")
+    List<Airport> findAirportsByPassId(@Param("passengerId") Long passengerId);
 }
