ed5456dc18fe6119bca62da65d887205349317bc
Cameron Beanland <cameronbeanland@hotmail.com>
2025-02-28 19:52:10 -0330

    Update AircraftRepo.java
    Added an ungodly amount of imports to each of the following, and added a custom query to help define relationships between them (interactions with each other)

--- server/src/main/java/com/example/demo/repository/AircraftRepo.java
+++ server/src/main/java/com/example/demo/repository/AircraftRepo.java
@@ -1,4 +1,15 @@
 package com.example.demo.repository;
 
-public class AircraftRepo {
+import com.example.demo.model.Aircraft;
+import com.example.demo.model.Airport;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface AircraftRepo extends JpaRepository<Aircraft, Long> {
+    // query to get airports an aircraft to travel in their direction (one airport to another, awful explanation lol)
+    @Query("SELECT a FROM Airport a JOIN a.aircrafts ac WHERE ac.id = :aircraftId")
+    List<Airport> findAirportsByAircraftId(@Param("aircraftId") Long aircraftId);
 }
