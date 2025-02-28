38625dc84d0e86487046a1329ef9da8678e90331
Cameron Beanland <cameronbeanland@hotmail.com>
2025-02-28 17:24:14 -0330

    Update City.java
    Imported lombok.Data and lombok.NoArgsConstructor to reduce file size and code length.

--- server/src/main/java/com/example/demo/model/City.java
+++ server/src/main/java/com/example/demo/model/City.java
@@ -5,11 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * This class is designed to represent cities that hold airports
  */
 @Entity  // @entity marks class as a JPA entity (which is linked to a database table)
+@Data // generate the mutator/accessors, toString, any hashCode and equals
+@NoArgsConstructor // generates a default constructor, to reduce code size
 public class City {
     @Id  // marking field as primary key
     @GeneratedValue(strategy = GenerationType.IDENTITY)  // auto-gen ID value
