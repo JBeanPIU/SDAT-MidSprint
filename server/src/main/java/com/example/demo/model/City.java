8b8e904cb374a3313732697d64423959b8a37fa3
Cameron Beanland <cameronbeanland@hotmail.com>
2025-02-28 20:59:15 -0330

    Update City.java

--- server/src/main/java/com/example/demo/model/City.java
+++ server/src/main/java/com/example/demo/model/City.java
@@ -1,13 +1,11 @@
 package com.example.demo.model;
 
 // imported jakarta instead of javax due to syntax errors on my end
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/* ========================================================== */
 /**
  * This class is designed to represent cities that hold airports
  */
