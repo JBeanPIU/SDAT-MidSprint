43687cbc902eec39c5cc6293803641f550caba82
Cameron Beanland <cameronbeanland@hotmail.com>
2025-02-28 20:58:25 -0330

    Update Aircraft.java

--- server/src/main/java/com/example/demo/model/Aircraft.java
+++ server/src/main/java/com/example/demo/model/Aircraft.java
@@ -1,13 +1,12 @@
 package com.example.demo.model;
 
 // imported jakarta instead of javax due to syntax errors on my end
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.util.List;
 
+/* ========================================================== */
 /**
  * Aircraft that allows Passengers to travel
  */
@@ -21,5 +20,7 @@ public class Aircraft {
     private String type;  // type of aircraft
     private String airlineName;  // company that owns airline
     private int numberOfPassengers;
-}
 
+    @ManyToMany(mappedBy = "aircrafts")
+    private List<Airport> airports;
+}
