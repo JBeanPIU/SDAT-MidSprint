5e8bf30648a63b05b5f9c5475ab1e646cc8303de
Cameron Beanland <cameronbeanland@hotmail.com>
2025-02-28 20:58:36 -0330

    Update Airport.java

--- server/src/main/java/com/example/demo/model/Airport.java
+++ server/src/main/java/com/example/demo/model/Airport.java
@@ -1,13 +1,13 @@
 package com.example.demo.model;
 
 // imported jakarta instead of javax due to syntax errors on my end
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Set;
+
+/* ========================================================== */
 /**
  * An airport class to store info on aircraft and where they travel
  */
@@ -20,4 +20,13 @@ public class Airport {
     private Long id;
     private String name;  // name of the airport
     private String code;  // signifies a unique airport code (like YYZ for Toronto Pearson for example)
+
+    @ManyToOne
+    @JoinColumn(name = "city_id")  // added field to represent city
+    private City city;
+
+    @ManyToMany
+    @JoinTable(name = "airport_aircraft", joinColumns = @JoinColumn(name = "airport_id"), inverseJoinColumns = @JoinColumn(name = "aircraft_id"))
+    private Set<Aircraft> aircrafts;
+
 }
