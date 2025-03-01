c74f65e0746adf4dc2d9a8a7d4ac52dcb7728ca0
Cameron Beanland <cameronbeanland@hotmail.com>
2025-02-28 21:00:10 -0330

    Update PassengerRepo.java

--- server/src/main/java/com/example/demo/repository/PassengerRepo.java
+++ server/src/main/java/com/example/demo/repository/PassengerRepo.java
@@ -6,9 +6,10 @@ import com.example.demo.model.Passenger;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 import java.util.List;
 
+/* ========================================================== */
+// purpose of repo classes is to allow db operations like saving, updating, etc. without making complex SQL queries manually lol
 public interface PassengerRepo extends JpaRepository<Passenger, Long> {
     // query to extract all aircraft a passenger has traveled on
     @Query("SELECT a FROM Aircraft a JOIN a.passengers p WHERE p.id = :passengerId")
@@ -17,4 +18,8 @@ public interface PassengerRepo extends JpaRepository<Passenger, Long> {
     // this query is used to get amount of airports a passenger has used
     @Query("SELECT a FROM Airport a JOIN a.passengers p WHERE p.id = :passengerId")
     List<Airport> findAirportsByPassId(@Param("passengerId") Long passengerId);
+
+    List<Aircraft> findAircraftsByPassengerId(Long passengerId);
+
+    List<Airport> findAirportsByPassengerId(Long passengerId);
 }
